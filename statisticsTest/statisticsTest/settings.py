e6098265bcf03651e8dfc1b9cbb95daa81da8927
TSteketee <49751519+TSteketee@users.noreply.github.com>
2021-10-29 13:46:18 +0200

    Add files via upload

--- statisticsTest/statisticsTest/settings.py
+++ statisticsTest/statisticsTest/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import django_heroku
 
 from pathlib import Path
 import os
@@ -133,3 +134,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+django_heroku.settings(locals())
